14efa8726d503ec407c779ea99c7453358092ba5
IYarosh <IYarosh@qulix.com>
2024-08-21 23:28:16 +0300

    Excercise 1

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,9 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState("");
+  const [selectedVariant, setSelectedVariant] = React.useState(VARIANT_OPTIONS[0]);
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +27,12 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={e => setMessage(e.currentTarget.value)}
+            />
           </div>
         </div>
 
@@ -33,17 +41,22 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map(variant => (
+              <label
+                key={variant}
+                htmlFor={`variant-${variant}`}
+              >
+                <input
+                  id={`variant-${variant}`}
+                  type="radio"
+                  name="variant"
+                  checked={variant === selectedVariant}
+                  value={variant}
+                  onChange={e => setSelectedVariant(e.currentTarget.value)}
+                />
+                {variant}
+              </label>
+            ))}
           </div>
         </div>
 
